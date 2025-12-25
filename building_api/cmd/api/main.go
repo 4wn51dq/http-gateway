b5449ec9715f5d012e3781c99d9b831aa7e0d863
awn <siddiqui4wn@gmail.com>
2025-12-25 13:47:57 +0530

    cli-api practise project

---
+++ building_api/cmd/api/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/avukadin/goapi/internal/handlers"
+	"github.com/go-chi/chi"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.SetReportCaller(true)
+	var r chi.Mux = chi.NewRouter() // chi.Mux is only a struct to set up api
+	handlers.Handler(r)
+	fmt.Println("Starting go api service..")
+	err := http.ListenAndServe("localhost:8000", r)
+	if err != nil {
+		log.Error(err)
+	}
+}
